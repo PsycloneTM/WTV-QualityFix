d09e6790f9ea7d9e5f4573ef65367868f54909aa
CycloneTM <ianbuddy278@gmail.com>
2026-06-02 23:49:09 +0000

    Update WTV Quality Selector Fix to version 1.2
    Refactor the user script to improve structuredClone and postMessage handling.

---
+++ WTV Quality Selector Fix-1.2.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         WTV Quality Selector Fix
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Fixes video quality selector crashes by handling Proxy cloning errors in structuredClone
+// @author       CycloneTM
+// @match        *://*.w.tv/*
+// @grant        none
+// @run-at       document-start
+// @updateURL    https://github.com/PsycloneTM/WTV-QualityFix/raw/refs/heads/main/WTV%20Quality%20Selector%20Fix-1.2.user.js
+// @downloadURL  https://github.com/PsycloneTM/WTV-QualityFix/raw/refs/heads/main/WTV%20Quality%20Selector%20Fix-1.2.user.js
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function deproxy(obj) {
+        try {
+            return JSON.parse(JSON.stringify(obj));
+        } catch (e) {
+            return obj;
+        }
+    }
+
+    const _clone = window.structuredClone;
+    window.structuredClone = function (obj, opts) {
+        try {
+            return _clone(obj, opts);
+        } catch (e) {
+            return deproxy(obj);
+        }
+    };
+
+    const _workerPostMessage = Worker.prototype.postMessage;
+    Worker.prototype.postMessage = function (msg, transfer) {
+        try {
+            // Attempt the original call first (fast path for non-proxy objects)
+            return _workerPostMessage.apply(this, arguments);
+        } catch (e) {
+            if (e instanceof DOMException) {
+                // Sanitise and retry
+                const clean = deproxy(msg);
+                return _workerPostMessage.call(this, clean, transfer || []);
+            }
+            throw e;
+        }
+    };
+})();
